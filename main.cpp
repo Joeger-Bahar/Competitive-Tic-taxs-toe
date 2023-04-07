179401c2c39bbe609b34c4e632e8280dbd43c87e
joshua-bar <123775427+joshua-bar@users.noreply.github.com>
2023-04-07 10:35:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,24 +9,24 @@
 char playerMark = 'X';
 short player = 1;
 
-char board[3][3] = {
+char board[9] = {
 	// The board that's printed out
-  	{'-', '-', '-'},
-	{'-', '-', '-'},
-	{'-', '-', '-'}
+  '-', '-', '-',
+	'-', '-', '-',
+	'-', '-', '-'
 };
 
 std::map<std::string, char> spots = {
 	// The board for inputting marks
-	{"A1", board[0][0]},
-	{"A2", board[1][0]},
-	{"A3", board[2][0]},
-	{"B1", board[0][1]},
-	{"B2", board[1][1]},
-	{"B3", board[2][1]},
-	{"C1", board[0][2]},
-	{"C2", board[1][2]},
-	{"C3", board[2][2]},
+	{"A1", board[0]},
+	{"B1", board[1]},
+	{"C1", board[2]},
+	{"A2", board[3]},
+	{"B2", board[4]},
+	{"C2", board[5]},
+	{"A3", board[6]},
+	{"B3", board[7]},
+	{"C3", board[8]},
 };
 
 void clearScreen() {
@@ -41,12 +41,12 @@ void Capitalize(std::string& str) {
 	}
 }
 
-void printBoard(const char (&board)[3][3]) {
+void printBoard(const char (&board)[9]) {
 	std::printf("    A   B   C\n");
-    	for (int i = 0; i < 8; i += 3) {
-        	printf("%i   %c | %c | %c\n", (i / 3 + 1), (*board)[i], (*board)[i+1], (*board)[i+2]);
-        	if (i < 5)
-            	printf("   -----------\n");
+    for (int i = 0; i < 8; i += 3) {
+        printf("%i   %c | %c | %c\n", (i / 3 + 1), board[i], board[i+1], board[i+2]);
+        if (i < 5)
+            printf("   -----------\n");
 	}
 }
 
@@ -55,55 +55,49 @@ void updateBoard() {
   const char columnLetters[] = {'A', 'B', 'C'};
   for (int row = 0; row < 3; row++) {
     for (int col = 0; col < 3; col++) {
-      board[row][col] = spots[columnLetters[col] + std::to_string(row + 1)];
+      board[col + row * 3] = spots[columnLetters[col] + std::to_string(row + 1)];
     }
   }
 }
 
-bool horizontalWin(const char type) {
-  int x, y;
+bool horizontalWin(const char type) { 
 	// Checks board for horizontal wins
-	std::set<char> row;
+  int won = 0;
+  int x, y;
 	for (int i = 0; i < 3; i++) {
-		row.clear();
 		for (int j = 0; j < 3; j++) {
-      			x = type == 'h' ? i : j;
-      			y = type == 'h' ? j : i;
-			row.insert(board[x][y]);
+      x = type == 'h' ? i : j;
+      y = type == 'h' ? j : i;
+      won -= board[x + y * 3] == 'X' ? -1 : board[x + y * 3] == 'O' ? 1 : 0;
 		}
-		if (row.size() == 1) {
-      			if (*row.begin() != '-') {
-        			return 1;
-      			}
-		}
+    if (abs(won) == 3) {
+      return 1;
+    }
+    won = 0;
 	}
 	return 0;
 }
 
 bool diagonalWin() {
 	// Checks board for wins diagonally
-	std::set<char> diagonal;
+  int won = 0;
 	for (int i = 0; i < 3; i++) {
-		diagonal.insert(board[i][i]);
+    won -= board[i + i * 3] == 'X' ? -1 : board[i + i * 3] == 'O' ? 1 : 0;
 	}
-	if (diagonal.size() == 1) {
-		if (*diagonal.begin() != '-') {
-      			return 1;
-    		}
-  	}
-	diagonal.clear();
+  if (abs(won) == 3) {
+    return 1;
+  }
+  won = 0;
 	for (int i = 0; i < 3; i++) {
-		diagonal.insert(board[i][2 - i]);
+    won -= board[i + (2 - i) * 3] == 'X' ? -1 : board[i + (2 - i) * 3] == 'O' ? 1 : 0;
 	}
-	if (diagonal.size() == 1) {
-		if (*diagonal.begin() != '-') {
-      			return 1;
-    		}
-	}
+  if (abs(won) == 3) {
+    return 1;
+  }
 	return 0;
 }
 inline bool win() {
-	if (diagonalWin() || horizontalWin('v') || horizontalWin('h')) {return 1;}
+  if (diagonalWin() || horizontalWin('v') || horizontalWin('h')) {return 1;}
 	return 0;
 }
 
@@ -112,18 +106,23 @@ inline void wonGame(const short player) {
 	std::printf("\nGood Job Player %i, You Won!\n", player);
 }
 
+void input(std::string& playerSpot, const char* statement) {
+  std::printf(statement, player);
+  std::cin >> playerSpot;
+  Capitalize(playerSpot);
+}
+
 int main()
 {
 	std::string playerSpot;
-  	auto input = [&](const char* statement) {std::printf(statement, player); std::cin >> playerSpot;Capitalize(playerSpot);};
-  	clearScreen();
-  	while (1) {
+  clearScreen();
+  while (1) {
 		printBoard(board);
-    		input("\nPlayer %i, choose a spot: ");
+    input(playerSpot, "\nPlayer %i, choose a spot: ");
 		while (!spots[playerSpot]) {
-			input("\nNot a valid spot. Please enter valid spot: ");
+			input(playerSpot, "\nNot a valid spot. Please enter valid spot: ");
 		}
-		
+
 		spots[playerSpot] = playerMark;
 		updateBoard();
 		clearScreen();
@@ -131,7 +130,7 @@ int main()
 			wonGame(player);
 			return 0;
 		}
-	  	player = (player == 2) ? 1 : 2;
+	  player = (player == 2) ? 1 : 2;
 		playerMark = (player == 1) ? 'X' : 'O';
 	}
 } 
